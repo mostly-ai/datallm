278f5deef1b6b9edd41b33a5166c5d4a18ad5f38
michdr <2467184+michdr@users.noreply.github.com>
2024-05-22 14:34:43 +0200

    build: better maintenance of versioning

--- datallm-client/datallm/__init__.py
+++ datallm-client/datallm/__init__.py
@@ -1,3 +1,5 @@
 from datallm._client import DataLLM
 from datallm._exceptions import APIError, APIStatusError
 from datallm._types import DtypeEnum
+
+__version__ = "0.2.3"  # Do not set this manually. Use poetry version [params]
