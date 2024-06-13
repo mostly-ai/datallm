ac58246fd206f8c08f422f968b90a47cfadc2655
michdr <2467184+michdr@users.noreply.github.com>
2024-06-13 13:12:49 +0200

    bump: to 0.2.4

--- datallm-client/datallm/__init__.py
+++ datallm-client/datallm/__init__.py
@@ -2,4 +2,4 @@ from datallm._client import DataLLM
 from datallm._exceptions import APIError, APIStatusError
 from datallm._types import DtypeEnum
 
-__version__ = "0.2.3"  # Do not set this manually. Use poetry version [params]
+__version__ = "0.2.4"  # Do not set this manually. Use poetry version [params]
